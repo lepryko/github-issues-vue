ec21fd5a12a8d333cd07ee38b4e507ec45ee9a06
jozzi <dominik.malantowicz@gmail.com>
2019-11-25 17:21:24 +0100

    Git add background image

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
+<style src="./styles.scss" lang="scss"/>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: 'app',
+    components: {},
+  };
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <div id="app">
+    <img class="background-img" alt="App background" src="./assets/background.jpg"/>
+    <div>
+      <span>Teest</span>
+    </div>
+  </div>
+</template>
